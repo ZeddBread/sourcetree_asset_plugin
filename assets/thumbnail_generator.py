2a9b06eaf55d5500930ebc0349b9ef989ee96aec
Ali Zreik <aliz@affirmer.com.au>
2024-07-15 15:16:16 +1000

    Revert "initial commit"
    This reverts commit 2c0e444e0a2b09c7e48e5229c0b93c70a42d1749.

--- assets/thumbnail_generator.py
+++
@@ -1,6 +0,0 @@
-from PIL import Image
-
-def generate_thumbnail(image_path, thumbnail_path, size=(128, 128)):
-    image = Image.open(image_path)
-    image.thumbnail(size)
-    image.save(thumbnail_path)
